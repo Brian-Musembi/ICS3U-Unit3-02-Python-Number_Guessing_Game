5527de6ab3fc79a73d8aa03f82784c30a807d0b1
Brian Musembi <brian.musembi@mths.ca>
2021-05-03 02:46:21 +0000

    added number_guessing_game

---
+++ number_guessing_game.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Brian Musembi
+# Created on: 03 May 2021
+# This program allows the user to guess the correct number
+
+
+def main():
+    # this function allows the user to guess the correct number
+
+    # input
+    guess = int(input("Enter a number between 0 - 9: "))
+
+    # process & output
+    if guess == 5:
+        print("You guessed correct!")
+    if guess != 5:
+        print("You guessed wrong!")
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
